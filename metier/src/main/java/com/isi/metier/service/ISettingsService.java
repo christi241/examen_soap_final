d4c9dcb063c0786d308edbae6f9c00989217b248
claye malounga <clayemalounga@gmail.com>
2025-08-19 19:38:26 +0100

    modification

--- metier/src/main/java/com/isi/metier/service/ISettingsService.java
+++ metier/src/main/java/com/isi/metier/service/ISettingsService.java
@@ -4,13 +4,22 @@ import com.isi.metier.dto.ClassDto;
 import com.isi.metier.dto.SectorDto;
 import com.isi.metier.entity.Sectors;
 import com.isi.metier.entity.Classes;
-import jakarta.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
 import java.util.List;
 
 @WebService
 public interface ISettingsService {
+
+    @WebMethod
     void addSector(Sectors sector);
+
+    @WebMethod
     void addClass(Classes classe);
-    List<ClassDto> getClassesBySector(long sectorId); // <-- Modifié
-    List<SectorDto> getAllSectors(); // <-- Modifié
+
+    @WebMethod
+    List<ClassDto> getClassesBySector(long sectorId);
+
+    @WebMethod
+    List<SectorDto> getAllSectors();
 }
--- soap/src/main/java/com/isi/soap/service/SettingsWebService.java
+++ soap/src/main/java/com/isi/soap/service/SettingsWebService.java
@@ -2,39 +2,34 @@ package com.isi.soap.service;
 
 import com.isi.metier.dto.ClassDto;
 import com.isi.metier.dto.SectorDto;
-import com.isi.metier.service.ISettingsService;
-import com.isi.metier.service.SettingsServiceImpl;
 import com.isi.metier.entity.Classes;
 import com.isi.metier.entity.Sectors;
-import jakarta.jws.WebMethod;
-import jakarta.jws.WebService;
+import com.isi.metier.service.ISettingsService;
+import com.isi.metier.service.SettingsServiceImpl;
+import javax.jws.WebService;
 import java.util.List;
 
-@WebService(endpointInterface = "com.samane.metier.service.ISettingsService")
+@WebService(endpointInterface = "com.isi.metier.service.ISettingsService")
 public class SettingsWebService implements ISettingsService {
 
     private ISettingsService settingsService = new SettingsServiceImpl();
 
     @Override
-    @WebMethod
     public void addSector(Sectors sector) {
         settingsService.addSector(sector);
     }
 
     @Override
-    @WebMethod
     public void addClass(Classes classe) {
         settingsService.addClass(classe);
     }
 
     @Override
-    @WebMethod
     public List<ClassDto> getClassesBySector(long sectorId) {
         return settingsService.getClassesBySector(sectorId);
     }
 
     @Override
-    @WebMethod
     public List<SectorDto> getAllSectors() {
         return settingsService.getAllSectors();
     }
